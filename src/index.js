fb540489c51a92ff145f72ff75be9782031b45fe
vladdengoit <114827862+vladdengoit@users.noreply.github.com>
2024-02-11 12:41:05 +0200

    base name

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <Provider store={store}>
     <PersistGate loading ={null} persistor={persistore}>
-     <BrowserRouter>
+     <BrowserRouter basename="/goit-react-hw-06-phonebook">
     <App />
     </BrowserRouter>
     </PersistGate>
